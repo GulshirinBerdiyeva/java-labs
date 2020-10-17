fbdf59a30719f8ee5ef7fa3b36f36251b1ddab1d
Gulshirin Berdiyeva <gulshirinberdiyeva01@gmail.com>
2020-10-17 15:33:24 +0300

    ADDED: fields of Mainframe

--- src/bsu/frct/java/lab2/MainFrame.java
+++ src/bsu/frct/java/lab2/MainFrame.java
@@ -4,6 +4,18 @@ import javax.swing.*;
 
 public class MainFrame extends JFrame {
 
+    private static final int WIDTH = 400,
+                             HEIGHT = 320;
+    private JTextField textFieldResult,
+                       textFieldX,
+                       textFieldY;
+    private ButtonGroup radioButtons = new ButtonGroup();
+    private Box hboxFormulaType = Box.createHorizontalBox();
+    private int formulaId = 1;
+
+
+
+
     public static void main(String[] args) {
 
     }
