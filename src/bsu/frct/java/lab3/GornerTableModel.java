76b30f159a1b1378ee6c02bd18c84529d3436b73
Gulshirin Berdiyeva <gulshirinberdiyeva01@gmail.com>
2020-10-31 20:54:07 +0300

    ADDED: new class GornerTableModel

---
+++ src/bsu/frct/java/lab3/GornerTableModel.java
@@ -0,0 +1,20 @@
+package bsu.frct.java.lab3;
+
+import javax.swing.table.AbstractTableModel;
+
+public class GornerTableModel extends AbstractTableModel {
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return null;
+    }
+}
