b5f6989e3a7c29584782ebe700c12664f7cdec01
Gulshirin Berdiyeva <gulshirinberdiyeva01@gmail.com>
2020-10-31 23:45:58 +0300

    ADDED: MainFrame class fields

--- src/bsu/frct/java/lab3/MainFrame.java
+++ src/bsu/frct/java/lab3/MainFrame.java
@@ -1,8 +1,24 @@
 package bsu.frct.java.lab3;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends Frame {
+    private static final int WIDTH = 1000,
+                             HEIGHT = 500;
+    private Double[] coefficients;
+    private JFileChooser fileChooser = null;
+    private JMenuItem saveToTextMenuItem,
+                      saveToGraphicsMenuItem,
+                      saveToCSVMenuItem,
+                      searchValueMenuItem,
+                      searchSimilarValueMenuItem;
+    private JTextField textFieldFrom,
+                       textFieldTo,
+                       textFieldStep;
+    private Box hBoxResult;
+    private HornerTableCellRenderer renderer = new HornerTableCellRenderer();
+    private HornerTableModel data;
 
     public static void main(String[] args) {
 
