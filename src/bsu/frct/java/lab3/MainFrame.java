70368221af6dad356b1c928ae19cd16b40de64a4
Gulshirin Berdiyeva <gulshirinberdiyeva01@gmail.com>
2020-11-01 16:06:29 +0300

    CORRECTED: extends JFrame instead of Frame

--- src/bsu/frct/java/lab3/MainFrame.java
+++ src/bsu/frct/java/lab3/MainFrame.java
@@ -1,39 +1,9 @@
 package bsu.frct.java.lab3;
 
 import javax.swing.*;
-import java.awt.*;
 
-public class MainFrame extends Frame {
-    private static final int WIDTH = 1000,
-                             HEIGHT = 500;
-    private Double[] coefficients;
-    private JFileChooser fileChooser = null;
-    private JMenuItem saveToTextMenuItem,
-                      saveToGraphicsMenuItem,
-                      searchValueMenuItem;
-    private JTextField textFieldFrom,
-                       textFieldTo,
-                       textFieldStep;
-    private Box hBoxResult;
-    private HornerTableCellRenderer renderer = new HornerTableCellRenderer();
-    private HornerTableModel data;
+public class MainFrame extends JFrame {
 
-    public MainFrame(Double[] coefficients){
-        super("Tabulating a polynomial on a segment in 2 ways..");
-        this.coefficients = coefficients;
-        setSize(WIDTH, HEIGHT);
-        Toolkit kit = Toolkit.getDefaultToolkit();
-        setLocation((kit.getScreenSize().width - WIDTH) / 2,
-                (kit.getScreenSize().height - HEIGHT) / 2);
-        JMenuBar menuBar = new JMenuBar();
-        JFrame frame = new JFrame();
-        frame.setJMenuBar(menuBar);
-        JMenu fileMenu = new JMenu("File");
-        menuBar.add(fileMenu);
-        JMenu tablaMenu = new JMenu("Table");
-        menuBar.add(tablaMenu);
-
-    }
 
     public static void main(String[] args) {
 
