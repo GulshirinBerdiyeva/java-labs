c113434c1428bdb88f97108a48133efdd1ee2890
Gulshirin Berdiyeva <gulshirinberdiyeva01@gmail.com>
2020-11-15 02:30:21 +0300

    ADDED: inner class

--- src/bsu/frct/java/lab4/MainFrame.java
+++ src/bsu/frct/java/lab4/MainFrame.java
@@ -1,6 +1,8 @@
 package bsu.frct.java.lab4;
 
 import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
 public class MainFrame extends JFrame {
     private static final int WIDTH = 1000,
@@ -8,11 +10,24 @@ public class MainFrame extends JFrame {
     private boolean fileLoaded = false;
     private GraphicsDisplay display = new GraphicsDisplay();
     private JCheckBoxMenuItem showAxisMenuItem,
-                              showMarkerMenuItem;
+                              showMarkersMenuItem;
     private JFileChooser fileChooser = null;
 
     public static void main(String[] args) {
 
     }
 
+    private class GraphicsMenuListener implements MenuListener{
+        @Override
+        public void menuSelected(MenuEvent e) {
+            showAxisMenuItem.setEnabled(fileLoaded);
+            showMarkersMenuItem.setEnabled(fileLoaded);
+        }
+
+        @Override
+        public void menuDeselected(MenuEvent e) {}
+
+        @Override
+        public void menuCanceled(MenuEvent e) {}
+    }
 }
