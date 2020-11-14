a76b408852eb0d38843b81a06fda13dfd92f92fc
Gulshirin Berdiyeva <gulshirinberdiyeva01@gmail.com>
2020-11-14 23:53:19 +0300

    ADDED: GraphicsDisplay fields

--- src/bsu/frct/java/lab4/GraphicsDisplay.java
+++ src/bsu/frct/java/lab4/GraphicsDisplay.java
@@ -1,8 +1,22 @@
 package bsu.frct.java.lab4;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GraphicsDisplay extends JPanel {
+    private Double[][] graphicsData;
+    private double minX,
+                   maxX,
+                   minY,
+                   maxY,
+                   scale;
+    private boolean showAxis = true,
+                    showMarkers = true;
+    private Font axisFont;
+    private BasicStroke axisStroke,
+                        graphicsStroke,
+                        markerStroke;
+
 
 
 }
