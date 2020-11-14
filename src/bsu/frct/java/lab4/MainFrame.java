7db2de4e49e0d7a4e69decc626b4c62f47d57dad
Gulshirin Berdiyeva <gulshirinberdiyeva01@gmail.com>
2020-11-15 02:26:29 +0300

    ADDED: MainFrame fields

--- src/bsu/frct/java/lab4/MainFrame.java
+++ src/bsu/frct/java/lab4/MainFrame.java
@@ -3,8 +3,16 @@ package bsu.frct.java.lab4;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
+    private static final int WIDTH = 1000,
+                             HEIGHT = 500;
+    private boolean fileLoaded = false;
+    private GraphicsDisplay display = new GraphicsDisplay();
+    private JCheckBoxMenuItem showAxisMenuItem,
+                              showMarkerMenuItem;
+    private JFileChooser fileChooser = null;
 
     public static void main(String[] args) {
 
     }
+
 }
